1d18b3d73eadae571d34062ec461be15fa95ca67
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-28 18:15:20 +0700

    Initial Commit

---
+++ QuizSound/QuizSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSoundApp.swift
+//  QuizSound
+//
+//  Created by emi oiso on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct QuizSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
